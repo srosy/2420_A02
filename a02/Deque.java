ad915991d68b72ecc87f5377e09c61b7c6d8c66e
srosy <serosenvall@gmail.com>
2018-09-17 12:29:22 -0600

    initial commit

---
+++ a02/Deque.java
@@ -0,0 +1,98 @@
+package a02;
+
+import java.util.Iterator;
+
+public class Deque<Item> implements Iterable<Item> {
+
+	/*
+	 * construct an empty deque
+	 */
+
+	public Deque() {
+		// TODO
+	}
+
+	/*
+	 * 
+	 * is the deque empty?*@return
+	 */
+
+	public boolean isEmpty() {
+		return false;
+		// TODO
+	}
+
+	/*
+	 * return
+	 * 
+	 * the number of items on the deque*@return
+	 */
+
+	public int size() {
+		return 0;
+		// TODO
+	}
+
+	/*
+	 * 
+	 * insert the item at the front*
+	 * 
+	 * @param item
+	 */
+	public void addFirst(Item item) {
+		// TODO
+	}
+
+	/*
+	 * 
+	 * insert the item at the end*
+	 * 
+	 * @param item
+	 */
+	public void addLast(Item item) {
+		// TODO
+	}
+
+	/*
+	 * 
+	 * delete and return the item at the front*@return
+	 */
+
+	public Item removeFirst() {
+		return null;
+		// TODO
+	}
+
+	/*
+	 * 
+	 * delete and return the item at the end*@return
+	 */
+
+	public Item removeLast() {
+		return null;
+		// TODO
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/*
+	 * return
+	 * 
+	 * an iterator over items in order from front to end*@return/
+	 * 
+	 * public Iterable<Item> iterator() { //TODO return null; }
+	 * 
+	 * /*******
+	 * 
+	 * Test Client
+	 *******/
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ a02/RandomizedQueue.java
@@ -0,0 +1,84 @@
+package a02;
+
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+	/*
+	 * construct an empty randomized queue
+	 **/
+
+	public RandomizedQueue() {
+		// TODO
+	}
+
+	/*
+	 * 
+	 * is the queue empty?*@return
+	 */
+
+	public boolean isEmpty() {
+		return false;// TODO
+
+	}
+
+	/*
+	 * return
+	 * 
+	 * the number of items on the queue*@return
+	 */
+
+	public int size() {
+		return 0;// TODO
+
+	}
+
+	/*
+	 * 
+	 * add the item*
+	 * 
+	 * @param item
+	 */
+	public void enqueue(Item item) {
+
+	}
+
+	/*
+	 * 
+	 * delete and return a random item*@return
+	 */
+
+	public Item dequeue() {
+		return null;// TODO
+
+	}
+
+	/*
+	 * return(but do
+	 * 
+	 * not delete) a random item*@return
+	 */
+
+	public Item sample() {
+		return null;// TODO
+
+	}
+
+	@Override
+	public Iterator<Item> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * return an independent iterator over items in random order
+	 * 
+	 * @return / public Iterator<Item> iterator() { //TODO }
+	 * 
+	 *         / * * * * * * * Test Client * * * * * *
+	 */
+	public static void main(String[] args) {
+		// TODO
+	}
+
+}
