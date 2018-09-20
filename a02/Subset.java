37a3514349cc2e720b275f50d92e778a3b0a1d3a
srosy <serosenvall@gmail.com>
2018-09-20 17:14:00 -0600

    subset.java done

--- a02/Subset.java
+++ a02/Subset.java
@@ -1,17 +1,34 @@
-package a02;
-
 /************************************************
  * Author(s): Gerald Brady, Spencer Rosenvall
  * Class: CSIS 2420
  * Professor: Margarethe Posch
  * Assignment: A02_RadomizedQueuesAndDeques
  ************************************************/
+package a02;
 
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * Class Subset is a test client for classes Deque.java and
+ * RandomizedQueue.java. Class demonstrates operation of RandomizedQueue by
+ * adding and removing Strings from the queue.
+ * 
+ * @author Gerald Brady, Spencer Rosenvall
+ *
+ */
 public class Subset {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String[] stringArray = StdIn.readAllStrings(); // read in all the strings
+		RandomizedQueue<String> rq = new RandomizedQueue<>();
+		int k = Integer.parseInt(args[0]);
 
+		for (String s : stringArray) { // Add String to queue.
+			rq.enqueue(s);
+		}
+
+		for (int i = 0; i < k; i++) { // Remove String from deque.
+			System.out.println(rq.dequeue());
+		}
 	}
-
 }
