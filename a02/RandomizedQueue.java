8796b29614e4a41d747a64acda6ba603320a71d7
spikegif <36247792+spikegif@users.noreply.github.com>
2018-09-20 20:51:31 -0600

    Update RandomizedQueue.java

--- a02/RandomizedQueue.java
+++ a02/RandomizedQueue.java
@@ -1,34 +1,36 @@
+package a02;
+
 /************************************************
  * Author(s): Gerald Brady, Spencer Rosenvall
  * Class: CSIS 2420
- * Professor: Frau Posch
+ * Professor: Margarethe Posch
  * Assignment: A02_RadomizedQueuesAndDeques
  ************************************************/
 
-package a02;
-
 import java.util.Iterator;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-
-	private int size;
-	private Item[] items;
-
+    private Item[] items; 
+    private int size = 0;
+    
 	/**
 	 * Constructs an empty randomized queue.
-	 */
+	 **/
 	public RandomizedQueue() {
-		items = (Item[]) new Object[1]; // min size for array is 1, cast as FAQ suggests
+		items = (Item[]) new Object[1]; //TODO how do we deal with the Type Safety?
 		size = 0;
+		
 	}
 
 	/**
 	 * Checks if the queue is empty.
-	 * 
+	 *
 	 * @return boolean
 	 */
 	public boolean isEmpty() {
-		return size == 0 ? true : false;
+		return size == 0;// TODO no need to add this: (? true : false;)
+
 	}
 
 	/**
@@ -37,51 +39,89 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 	 * @return int
 	 */
 	public int size() {
-		return 0;// TODO
+		return size;
 
 	}
-
+	
 	/**
 	 * Adds an item to the queue.
 	 * 
 	 * @param item
 	 */
 	public void enqueue(Item item) {
-		// TODO
-		// check end of list ? create new array with array.length * 2.
-		// add item to queue.
+		if (item == null)
+			throw new NullPointerException();
+		
+		if (size == items.length)
+			resize(items.length * 2);
+		
+		items[size] = item;
+		size++;
+		
 	}
-
+	
 	/**
 	 * Deletes and returns a random item.
-	 * 
+	 *
 	 * @return Item
 	 */
 	public Item dequeue() {
-		// TODO
-		// check if size <= (array.length / 4) ? create new array with half size.
-		// return the item dequeued.
-		return null;
-
+		if (isEmpty())
+			throw new java.util.NoSuchElementException();
+		
+		int random = StdRandom.uniform(size);
+		Item item = items[random];
+		
+		if (random != size -1)
+			items[random] = items[size - 1];
+		
+		// Loitering removal?  is this how I do it??
+		items[size - 1] = null;
+		size--;
+		
+		// resize smaller at 1/4
+		if (size > 0 && size == (items.length / 4))
+			resize(items.length / 2);
+		
+		return item;
+		
 	}
-
+	
 	/**
 	 * Returns (but does not delete) a random item.
 	 * 
 	 * @return Item
 	 */
 	public Item sample() {
-		// select a random item.
-		return null;// TODO
+		//TODO do we need to add this if it is empty?
+//		if (isEmpty())
+//			throw new java.util.NoSuchElementException();
+			
+		int random = StdRandom.uniform(size);
+		Item item = items[random];
+		
+		return item;
 
 	}
+	
+	//TODO we can add a resize method, right??
+    private void resize(int newSize) {
+        Item tempItems[] = (Item[]) new Object[newSize];//TODO how do we deal with the Type Safety?
 
+        for (int i = 0; i < size; i++) {//TODO check to see if size (i < size) should be (i < newSize)
+        	tempItems[i] = items[i];
+        }
+        items = tempItems;
+
+    }	
+	
 	/**
 	 * Returns an independent iterator over items in random order
 	 */
 	@Override
 	public Iterator<Item> iterator() {
 		// TODO Auto-generated method stub
+		//throw a java.lang.UnsupportedOperationException if the client calls the remove() method in the iterator
 		return null;
 	}
 
